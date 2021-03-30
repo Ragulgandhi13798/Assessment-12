407cf1d2024094337f0fd633965df8558819e855
RagV2C18472 <RagV2C18472@V2CP0082.vee2office.com>
2021-03-30 08:05:29 +0530

    Assessment-12

---
+++ mapcollection/src/map/test/HashMapExample1.java
@@ -0,0 +1,22 @@
+package map.test;
+
+import java.util.*;  
+
+public class HashMapExample1
+{  
+	
+ public static void main(String args[])
+ {  
+   HashMap<Integer,String> map=new HashMap<Integer,String>();    
+   map.put(1,"Mango");
+   map.put(2,"Apple");    
+   map.put(3,"Banana");   
+   map.put(4,"null");   
+   map.put(null,"null");   
+   map.put(5,"null");
+   map.put(null,"jhht");
+   System.out.println(map);  
+    
+   
+   }  
+}  
